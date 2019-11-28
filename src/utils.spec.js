c9f2a785a7b45c0b9da075e32c054956ecaf1e42
Chris Wray <chriswray76@gmail.com>
2019-11-28 20:52:08 +0000

    buildQueryString tested for query object of one key pair

--- src/utils.spec.js
+++ src/utils.spec.js
@@ -3,9 +3,19 @@ const { expect } = require('chai');
 const { buildQueryString } = require('./utils/utils');
 
 describe('', () => {
+  it('returns a string when passed nothing', () => {
+    const actual = buildQueryString();
+    const expected = '';
+    expect(actual).to.equal(expected);
+  });
   it('returns a string when passed an object', () => {
     const actual = buildQueryString({});
     const expected = '';
     expect(actual).to.equal(expected);
-  })
+  });
+  it('returns a query string starting with ? when passed an object with one query', () => {
+    const actual = buildQueryString({ term: 'Harry Potter' });
+    const expected = '?term=Harry Potter';
+    expect(actual).to.equal(expected);
+  });
 });
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,6 +1,11 @@
 const buildQueryString = ( queryObject ) => {
   if (!queryObject) return '';
-  return '';
+  let queryString = '';
+  const queryKeys = Object.keys(queryObject);
+  queryKeys.forEach(queryKey => {
+    queryString = `?${queryKey}=${queryObject[queryKey]}`
+  })
+  return queryString;
 }
 
 module.exports = { buildQueryString }
